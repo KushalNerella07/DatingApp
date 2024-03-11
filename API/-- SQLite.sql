572de77718179074709dbc8e595b5b72677cff92
Kushal Kumar Nerella <kushalkumarnerella@gmail.com>
2024-03-11 15:25:24 -0500

    End of Section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,7 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Bob");
+INSERT INTO Users (Id, UserName)
+VALUES (2,"John");
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Jane");
---
+++ API/Program.cs
@@ -0,0 +1,25 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+
+app.Run();
+
+
